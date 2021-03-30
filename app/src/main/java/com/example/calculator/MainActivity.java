83c9404a415b7f21db556563b62050735ec8ad79
AnnaShant <anna.lunatic.t@gmail.com>
2021-03-31 01:31:37 +0500

    Выполненная лабораторная

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -11,12 +11,9 @@ import android.widget.TextView;
 import java.math.RoundingMode;
 import java.text.DecimalFormat;
 
-/**
- *
- */
 public class MainActivity extends AppCompatActivity {
 
- //Кнопки
+ //Кнопки с числами
  Button mBtn0;
  Button mBtn1;
  Button mBtn2;
@@ -28,17 +25,24 @@ public class MainActivity extends AppCompatActivity {
  Button mBtn8;
  Button mBtn9;
 
+ //Кнопки операторов
  Button mBtnAddition;
  Button mBtnMultiplication;
  Button mBtnSubtraction;
  Button mBtnDivision;
-
- Button mBtnC;
- Button mBtnBack;
- Button mBtnPlusMinus;
- Button mBtnComma;
+ //Кнопка равно
  Button mBtnEqually;
 
+ //Кнопка очистки
+ Button mBtnC;
+ //Кнопка удаления последнего символа
+ Button mBtnBack;
+ //Кнопка "+/-"
+ Button mBtnPlusMinus;
+ //Кнопка с точкой
+ Button mBtnComma;
+
+ //Поле ввода
  TextView mField;
 
  float mValue = 0;
@@ -49,6 +53,7 @@ public class MainActivity extends AppCompatActivity {
   super.onCreate(savedInstanceState);
   setContentView(R.layout.activity_main);
 
+  //Кнопки с числами
   mBtn0 = findViewById(R.id.btn0);
   mBtn1 = findViewById(R.id.btn1);
   mBtn2 = findViewById(R.id.btn2);
@@ -60,20 +65,29 @@ public class MainActivity extends AppCompatActivity {
   mBtn8 = findViewById(R.id.btn8);
   mBtn9 = findViewById(R.id.btn9);
 
+  //Кнопки операторов
   mBtnAddition = findViewById(R.id.btnAddition);
   mBtnMultiplication = findViewById(R.id.btnMultiplication);
   mBtnSubtraction = findViewById(R.id.btnSubtraction);
   mBtnDivision = findViewById(R.id.btnDivision);
+  //Кнопка равно
   mBtnEqually = findViewById(R.id.btnEqually);
 
+  //Кнопка очистки
   mBtnC = findViewById(R.id.btnC);
+  //Кнопка удаления последнего символа
   mBtnBack = findViewById(R.id.btnBack);
+  //Кнопка "+/-"
   mBtnPlusMinus = findViewById(R.id.btnPlusMinus);
+  //Кнопка с точкой
   mBtnComma = findViewById(R.id.btnComma);
 
+  //Поле ввода
   mField = findViewById(R.id.field);
 
-
+  /**
+   Обработка кнопок с числами
+   * */
   View.OnClickListener numberListener = new View.OnClickListener() {
    @Override
    public void onClick(View v) {
@@ -92,7 +106,6 @@ public class MainActivity extends AppCompatActivity {
     mField.setText(field);
    }
   };
-
   //Подписки
   mBtn0.setOnClickListener(numberListener);
   mBtn1.setOnClickListener(numberListener);
@@ -110,41 +123,37 @@ public class MainActivity extends AppCompatActivity {
    public void onClick(View v) {
     onOperatorListener(v);
    }
-
    public void onOperatorListener(View button) {
-    //1
     String operator = ((Button) button).getText().toString();
     mOperator = operator;
 
-    //2
     String field = mField.getText().toString();
     mValue = Float.parseFloat(field);
 
-    //3
     mField.setText("0");
    }
   };
 
+  //Подписки
   mBtnAddition.setOnClickListener(operatorListener);
   mBtnMultiplication.setOnClickListener(operatorListener);
   mBtnSubtraction.setOnClickListener(operatorListener);
   mBtnDivision.setOnClickListener(operatorListener);
 
+  /**
+   Обработка кнопок с операторами и кнопки равно
+   * */
   View.OnClickListener resultListener = new View.OnClickListener() {
-
    @Override
    public void onClick(View v) {
     onResultListener(v);
    }
-
    public void onResultListener(View button) {
-    //1
-    String display = mField.getText().toString();
-    float value = Float.parseFloat(display);
+    String field = mField.getText().toString();
+    float value = Float.parseFloat(field);
     boolean i = true;
-    //2
     float result = value;
-    //3
+
     switch (mOperator) {
      case "+": {
       result = value + mValue;
@@ -168,22 +177,96 @@ public class MainActivity extends AppCompatActivity {
       break;
      }
     }
-    //4
     DecimalFormat format = new DecimalFormat("0.######");
     format.setRoundingMode(RoundingMode.DOWN);
     String resultText = format.format(result);
 
-    //5
     mField.setText(resultText);
-    //6
     mValue = result;
-    //7
     mOperator = "";
    }
   };
-    mBtnEqually.setOnClickListener(resultListener);
-  }
+  //Подписки
+  mBtnEqually.setOnClickListener(resultListener);
+
+  /**
+   Обработка кнопки с "+/-"
+   * */
+  View.OnClickListener plusMinusListener = new View.OnClickListener() {
+   @Override
+   public void onClick(View v) {
+    onPlusMinusListener(v);
+   }
+   public void onPlusMinusListener(View button) {
+    String display = mField.getText().toString();
+    float value = Float.parseFloat(display);
+    value = value*-1;
+
+    DecimalFormat format = new DecimalFormat("0.######");
+    format.setRoundingMode(RoundingMode.DOWN);
+    String resultText = format.format(value);
+
+    mField.setText(String.valueOf(resultText));
+   }
+  };
+  //Подписки
+  mBtnPlusMinus.setOnClickListener(plusMinusListener);
+
+  /**
+   Обработка кнопки очистки поля
+   * */
+  View.OnClickListener clearListener = new View.OnClickListener() {
+   @Override
+   public void onClick(View v) {
+    onClearListener(v);
+   }
+   public void onClearListener(View button) {
+    mValue = 0;
+    mOperator = "";
+    mField.setText("0");
+   }
+  };
+  //Подписки
+  mBtnC.setOnClickListener(clearListener);
+
+  /**
+   Обработка кнопки удаления последнего символа
+   * */
+  View.OnClickListener backListener = new View.OnClickListener() {
+   @Override
+   public void onClick(View v) {
+    onBackListener(v);
+   }
+   public void onBackListener(View button) {
+    String field = mField.getText().toString();
+    if (field.length() >1 ) {
+     field = field.substring(0, field.length() - 1);
+     mField.setText(field);
+    }
+    else if (field.length() <=1 ) {
+     mField.setText("0");
+    }
+   }
+  };
+  //Подписки
+  mBtnBack.setOnClickListener(backListener);
+
+  /**
+   Обработка кнопки с точкой
+   * */
+  View.OnClickListener commaListener = new View.OnClickListener() {
+   @Override
+   public void onClick(View v) {
+    onCommaListener(v);
+   }
+   public void onCommaListener(View button) {
+    String field = mField.getText().toString();
+    String display = field + ".";
+    mField.setText(display);
+   }
+  };
+  //Подписки
+   mBtnComma.setOnClickListener(commaListener);
  }
-
-
+}
 
